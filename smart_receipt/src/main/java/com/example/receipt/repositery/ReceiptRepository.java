ca36cb128c2db1ea994133b766782d04e10ac314
vaishalinatesamoorthy <144825365+vaishalinatesamoorthy@users.noreply.github.com>
2023-09-13 16:50:07 +0530

    Delete smart_receipt directory

--- smart_receipt/src/main/java/com/example/receipt/repositery/ReceiptRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.receipt.repositery;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.receipt.model.Receipt;
-
-public interface ReceiptRepository extends CrudRepository<Receipt,Long> {
-
-}
